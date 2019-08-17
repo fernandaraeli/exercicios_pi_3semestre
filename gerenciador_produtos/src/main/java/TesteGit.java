04a407a993705d61c18fc82915299857302dd4cf
fernanda.mhraeli <fernanda.mhraeli@CAS555517W1018.senacsp.edu.br>
2019-08-16 21:36:50 -0300

    Versao inicial system.out

---
+++ gerenciador_produtos/src/main/java/TesteGit.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author fernanda.mhraeli
+ */
+public class TesteGit {
+    public static void main(String[] args) {
+            System.out.println(" TESTE GIT");
+    }
+    
+}
